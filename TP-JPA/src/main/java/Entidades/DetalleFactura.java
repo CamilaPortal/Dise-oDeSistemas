5ed67570bb884a2b11b77b81037f58e653c6c898
CamilaPortal <c.portal@alumno.um.edu.ar>
2024-09-11 04:10:59 -0300

    tp-jpa

---
+++ TP-JPA/src/main/java/Entidades/DetalleFactura.java
@@ -0,0 +1,31 @@
+package Entidades;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+@Entity
+
+public class DetalleFactura implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int cantidad;
+    private int subTotal;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "fk_factura")
+    private Factura factura;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "fk_articulo")
+    private Articulo articulo;
+}
